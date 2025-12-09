04f87b442a09684b7e79ee9309f7f8d5f434ea7d
Antonio Neto <antonio.neto@colmeia.me>
2025-12-09 13:21:55 -0300

    versao 2.0

--- main.py
+++ main.py
@@ -1,127 +1,37 @@
 import pandas as pd
 import os
-import requests
-import time
-import tkinter as tk
-from tkinter import filedialog
-import questionary
 from tqdm import tqdm
 from datetime import datetime
-from openai import OpenAI
 from dotenv import load_dotenv
+#from fpdf import FPDF
+#from PIL import Image
+#from io import BytesIO
+
+from utilities.defs import get_user_id_by_vanity, get_user_info, get_owned_games, get_game_achievements, save_file_opt, ai_achievement_breakdown, save_to_sqlite
 
 load_dotenv()
+
+#76561199490364483 neto
+#76561197985622277 theo
+
+#------------------------------------Generic info
 API_KEY = os.getenv('API_KEY')
-
-#------------------------------------Generic functions
-
-def make_request(url, retry=4, wait=2):
-    """
-    Make a GET request to the specified URL with retry logic.
-    
-    Parameters:
-    - url (str): The URL to make the request to.
-    - retry (int): Number of retries in case of failure.
-    - wait (int): Seconds to wait before retrying.
-    
-    Returns:
-    - response (requests.Response): The response object from the request.
-    """
-    for attempt in range(retry):
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                return response
-            elif response.status_code == 429:
-                print(f'Rate limit exceeded. Waiting for {wait} seconds before retrying...')
-                time.sleep(wait)
-            else:
-                print(f'Error fetching data: {response.status_code}')
-                break
-        except requests.exceptions.RequestException as e:
-            print(f'Error connecting to API: {e}')
-    
-    print(f'[make_resquest] failed to connect to API')
-    return None
-
+ai_api_key=os.getenv("GPT_API_KEY")
 #------------------------------------user identification
 
-def get_user_id_by_vanity(api_key):
-    """
-    Get Steam user ID by vanity URL.
-    
-    Parameters:
-    - api_key (str): The API key for Steam API.
-    
-    Returns:
-    - steam_user_id (str): The Steam user ID.
-    """
-
-    print("Welcome to the Steam Data Extractor!")
-    print("This tool allows you to extract Steam user data and game achievements.\n")
-    print("Please select how you want to identify the user:")
-
-    while True:
-        try:
-            input_option = int(input('Select an option:\n1. By Steam ID\n2. By Vanity URL\n'))
-            
-            if input_option in(1, 2):
-                break
-            else:
-                print('Invalid option. Please select 1 or 2.')
-        except ValueError:
-            print('Invalid input. Please enter a number.')
-
-    if input_option == 1:
-        steam_user_id = input('Enter the Steam ID (ex 7656119...): ')
-        print(f'You selected to identify by Steam ID: {steam_user_id}')
-        return steam_user_id
-    elif input_option == 2:
-        vanity_name = input('Please enter the name present in your profile URL: ')
-        vanity_url = f'https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={api_key}&vanityurl={vanity_name}'
-
-        response = make_request(vanity_url, retry=4, wait=2)
-
-        if response is not None and response.status_code == 200:
-            json_vanity_info = response.json()
-            if json_vanity_info.get('response').get('success') == 1:
-                steam_user_id = json_vanity_info.get('response').get('steamid')
-                return steam_user_id
-        else:
-            print(f'Error: Vanity URL not found or does not exist. Please check the vanity name: {vanity_name} or use your Steam ID.')
-            return None
-        
-        
 steam_user_id = get_user_id_by_vanity(API_KEY)
 
-#------------------------------------Get all user informations by steam id
+if steam_user_id is None:
+    print("Please provide a valid Steam user ID or vanity name.")
+    exit()
+else:
+    print(f"Steam User ID identified: {steam_user_id}")
 
-def get_user_info(steam_user_id, api_key):
-    """
-    Get user information from Steam API by user ID.
-    
-    Parameters:
-    - steam_user_id (str): The Steam user ID.
-    - api_key (str): The API key for Steam API.
-    
-    Returns:
-    - json_user_info (dict): The JSON response containing user information.
-    """
-    
-    url = f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={api_key}&steamids={steam_user_id}'
-    response = make_request(url, retry=4, wait=2)
-
-    if response is not None and response.status_code == 200:
-        json_user_info = response.json()
-        return json_user_info
-    else:
-        print(f'Error fetching user information for Steam ID {steam_user_id}. Please check the ID or your API key.')
-        return None
-
+print("Start extracting information...")
 
 players_info = get_user_info(steam_user_id, API_KEY).get('response').get('players')[0]
-df_steam_user = pd.DataFrame([players_info])
 
+df_steam_user = pd.DataFrame([players_info])
 
 df_user_final = pd.DataFrame(
     {
@@ -134,39 +44,16 @@ df_user_final = pd.DataFrame(
     'timecreated': df_steam_user['timecreated'].apply(lambda x: datetime.fromtimestamp(x)) if 'timecreated' in df_steam_user.columns else None,
     'lastlogoff': df_steam_user['lastlogoff'].apply(lambda x : datetime.fromtimestamp(x)) if 'lastlogoff' in df_steam_user.columns else None,
     'loccountrycode': df_steam_user['loccountrycode'] if 'loccountrycode' in df_steam_user.columns else None,
-    'avatarmedium': df_steam_user['avatarmedium']
+    'avatarmedium': df_steam_user['avatarmedium'],
+    'dh_updated': datetime.now()
     }
 )
 
 if df_user_final['communityvisibilitystate'].iloc[0] != 3:
-    print(f'Hello {df_user_final['personaname']} profile is private, cannot fetch games. Please make it public to continue.')
+    print(f'{df_user_final['personaname']} profile is private, cannot fetch games. Please make it public to continue.')
     exit()
 
 #------------------------------------Get list of owned games
-
-def get_owned_games(steam_user_id, api_key):
-    """
-    Get the list of owned games by a Steam user.
-    
-    Parameters:
-    - steam_user_id (str): The Steam user ID.
-    - api_key (str): The API key for Steam API.
-    
-    Returns:
-    - json_games_info (dict): The JSON response containing game information.
-    """
-    
-    games_info_url = f'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={steam_user_id}&include_appinfo=true&include_played_free_games=true&format=json'
-    
-    response = make_request(games_info_url, retry=4, wait=2)
-
-    if response is not None and response.status_code == 200:
-        json_games_info = response.json()
-        return json_games_info
-    else:
-        print(f'Error fetching owned games for Steam ID {steam_user_id}. Please check the ID or your API key.')
-        return None
-
 
 games_info = get_owned_games(steam_user_id, API_KEY).get('response').get('games')
 
@@ -174,14 +61,7 @@ if games_info is None:
     print(f'No games found for Steam ID {steam_user_id}. Please check the ID or if you profile is public.')
     exit()
 
-game_list = []
-
-for game in enumerate(games_info):
-    game_datails = game[1]
-
-    game_list.append(game_datails)
-
-df_game_information = pd.DataFrame(game_list)
+df_game_information = pd.DataFrame(games_info)
 
 df_game_information['steam_player_id'] = steam_user_id
 df_game_information_filtered = df_game_information[df_game_information['playtime_forever'] > 0]
@@ -193,10 +73,10 @@ df_game_information_final = pd.DataFrame(
         'name': df_game_information_filtered['name'],
         'last_played_timestamp': df_game_information_filtered['rtime_last_played'].apply(lambda x: datetime.fromtimestamp(x)) if 'rtime_last_played' in df_game_information_filtered.columns else None,
         'playtime_forever': df_game_information_filtered['playtime_forever'],
-        'img_icon_url': df_game_information_filtered['img_icon_url'],
+        'img_game_cover_url': df_game_information_filtered['img_icon_url'],
         'has_community_visible_stats': df_game_information_filtered['has_community_visible_stats'],
         'playtime_2weeks': df_game_information_filtered['playtime_2weeks'] if 'playtime_2weeks' in df_game_information_filtered.columns else None,
-
+        'dh_updated': datetime.now()
     } 
 )
 
@@ -205,32 +85,8 @@ df_game_information_final = pd.DataFrame(
 collect_achievements_info = []
 no_information_games = []
 
-def get_game_achievements(steam_user_id, api_key, appid):
-    """
-    Get the achievements for a specific game by appid.
-    
-    Parameters:
-    - steam_user_id (str): The Steam user ID.
-    - api_key (str): The API key for Steam API.
-    - appid (int): The application ID of the game.
-    
-    Returns:
-    - json_game_achievements (dict): The JSON response containing game achievements.
-    """
-    
-    game_achievements_url = f'http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid={appid}&key={api_key}&steamid={steam_user_id}&l=en'
-    response = make_request(game_achievements_url, retry=4, wait=2)
-
-    if response is not None and response.status_code == 200:
-        json_game_achievements = response.json()
-        return json_game_achievements
-    else:
-        print(f'Error fetching achievements for appid {appid}. Please check the appid or your API key.')
-        return None
-
-
-for indesx, acvt in tqdm(df_game_information_final.iterrows(), desc="Fetching achievements", total=len(df_game_information_final), ncols=100):
-    selected_appid = acvt['steam_game_id']
+for indesx, game in tqdm(df_game_information_final.iterrows(), desc="Fetching achievements", total=len(df_game_information_final), ncols=100):
+    selected_appid = game['steam_game_id']
 
     game_achievements_url = f'http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid={selected_appid}&key={API_KEY}&steamid={steam_user_id}'
 
@@ -260,125 +116,48 @@ df_achievements_raw = pd.DataFrame(
         'total_game_acheivements_unlocked': df_achievements['achievements'].apply(lambda x: sum(item['achieved'] for item in x) if isinstance(x, list) else 0)
     }
 )
+#--------------------------------------------------- Joining dataframes
 
 df_achievements_final = df_achievements_raw.drop_duplicates(subset=['steam_game_id'])
 
 df_game_status = pd.merge(df_game_information_final, df_achievements_final, on='steam_game_id', how='inner')
 
-df_join_w_game = df_game_status.drop(columns=['steam_user_id_y', 'has_community_visible_stats','appid'])
-df_join_w_user = pd.merge(df_user_final, df_join_w_game, left_on='steamid', right_on='steam_user_id_x', how='inner')
+df_final = df_game_status.drop(columns=['steam_user_id_y', 'has_community_visible_stats','appid'])
 
-df_final = df_join_w_user.drop(columns=['steam_user_id_x', 'communityvisibilitystate', 'profilestate', 'avatarmedium'])
-
-player_name = df_final['personaname'].iloc[0]
-
-file_name = f"{player_name}_steam_data_{datetime.now().strftime('%Y%m%d')}"
-file_name = file_name.replace(' ', '_')
+#------------------------------------Get all user informations by steam id
 
 while True:
-    option = int(input("Select an option:\n1. Save as CSV\n2. Save as Excel\n3. Save as JSON\n"))
-    if option in [1, 2, 3]:
+    action = int(input(
+        """
+        select one of the available options:
+            \n1. Extract file with informations\n2. Extract personal documentation\n3. Tips for achievements (AI)\n4. Save data to local database
+        """))
+    if action in [1, 2, 3, 4]:
         break
     else:
         print("Invalid option. Please select 1, 2, or 3.")
 
-cust_path = input("Do you want to save the file in a custom path? (yes/no): ").lower()
+if action == 1:
 
-root = tk.Tk()
+#--------------------------------------------------- Building file name
 
-if cust_path == 'yes':
-    file_path = filedialog.askdirectory(title="Select a directory")
-    print(f"Selected directory: {file_path}")
-else:
-    print("Using current directory for saving files.")
+    player_name = df_user_final['personaname'].iloc[0]
+    file_name = f"{player_name}_steam_data_{datetime.now().strftime('%Y%m%d')}"
+    file_name = file_name.replace(' ', '_')
+
+    save_file_opt(df_final, file_name)
+    print(f"Data saved successfully as {file_name}.")
+
+#------------------------------------#Chat gpt to searching not unlock achievements #
+
+if action == 3:
+    ai_achievement_breakdown(ai_api_key, steam_user_id, API_KEY)
+    
+#------------------------------------#Save data to local sqlite database #
+
+if action == 4:
+    save_to_sqlite(df_final, table_name='collection_game_data', method='append')
+    save_to_sqlite(df_user_final, table_name='profile_data', method='append')
 
 
-if option == 1:
-    if cust_path == 'yes':
-        df_final.to_csv(f"{file_path}/{file_name}.csv", index=False)
-    else:
-        df_final.to_csv(f"{file_name}.csv", index=False)
-elif option == 2:
-    if cust_path == 'yes':
-        df_final.to_excel(f"{file_path}/{file_name}.xlsx", index=False)
-    else:
-        df_final.to_excel(f"{file_name}.xlsx", index=False)
-elif option == 3:
-    if cust_path == 'yes':
-        df_final.to_json(f"{file_path}/{file_name}.json", orient='records', lines=True)
-    else:
-        df_final.to_json(f"{file_name}.json", orient='records', lines=True)
 
-#------------------------------------#Chat gpt to search not unlock achievements #
-
-tips_option = str(input("Would you like to extract a list of needes to beat the game achievements? (yes/no):"))
-
-if tips_option.lower() == 'yes':
-
-    options = df_game_information_final['name'].tolist()
-    options.sort()
-
-    selected_option = questionary.select(
-        "Select a game to view unlocked achievements breakdonw:",
-        choices=options 
-    ).ask()
-
-    appid = df_game_information_final[df_game_information_final['name'] == selected_option]['steam_game_id'].iloc[0]
-
-    print(f"You selected: {selected_option}({appid})")
-
-    json_game_breakdown = get_game_achievements(steam_user_id, API_KEY, appid)
-
-    client = OpenAI(
-        api_key=os.getenv("GPT_API_KEY")
-    )
-
-    prompt = f"""
-    You are a famous and respected video game journalist who specializes in helping players achieve 100% completion, platinum trophies, or 1000G achievements.
-
-    I will provide you with:
-
-    - The name of a game
-    - A JSON file containing all achievements in this game, indicating which ones the player has unlocked and which ones remain locked.
-
-    unlocked achievements is set to true, and locked achievements is set to false.
-
-    For each locked achievement, please provide:
-
-    1. The achievement name.
-    2. A brief summary of what the player needs to do to unlock it.
-    3. Helpful tips to achieve it.
-
-    If an achievement has no description in the JSON, search for official information on the internet (e.g., game wikis, official sources).  
-    - If you find reliable info, include it as the summary and tips.  
-
-    for each achievement, please provide the information in the following format:
-    - Achievement Name: [Achievement Name]
-    - Summary: [Brief summary of what the player needs to do to unlock it]
-    - Tips: [Helpful tips to achieve it]
-
-    Please provide your response clearly and concisely, as if you are preparing a guide to be shown inside a gaming app.
-
-    Here is the game name and the JSON data:
-
-    Game: {selected_option}
-
-    Achievements JSON:  
-    {json_game_breakdown.get('playerstats').get('achievements')}
-    """
-
-    completion = client.chat.completions.create(
-        model=os.getenv("GPT_MODEL"),
-        store=True,
-        temperature=0.5,
-        max_tokens=800,
-        messages=[
-            {"role": "user", "content": prompt }
-        ]
-    )
-
-    content = completion.choices[0].message.content
-    print(content)
-    content.to_csv(f"{file_path}/{file_name}_breakdown.csv", index=False)
-elif tips_option.lower() == 'no':
-    print("No breakdown will be provided.")
---
+++ utilities/defs.py
@@ -0,0 +1,317 @@
+import requests
+import os
+import pandas as pd
+import time
+import tkinter as tk
+from tkinter import filedialog
+import questionary
+import sqlite3
+from openai import OpenAI
+
+
+
+def make_request(url, retry=4, wait=2):
+    """
+    Make a GET request to the specified URL with retry logic.
+    
+    Parameters:
+    - url (str): The URL to make the request to.
+    - retry (int): Number of retries in case of failure.
+    - wait (int): Seconds to wait before retrying.
+    
+    Returns:
+    - response (requests.Response): The response object from the request.
+    """
+    for attempt in range(retry):
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                return response
+            elif response.status_code == 429:
+                print(f'Rate limit exceeded. Waiting for {wait} seconds before retrying...')
+                time.sleep(wait)
+            else:
+                print(f'Error fetching data: {response.status_code}')
+                break
+        except requests.exceptions.RequestException as e:
+            print(f'Error connecting to API: {e}')
+    
+    print(f'[make_resquest] failed to connect to API')
+    return None
+
+#----------------------------------------------------------------------
+
+def get_user_id_by_vanity(api_key):
+    """
+    Get Steam user ID by vanity URL.
+    
+    Parameters:
+    - api_key (str): The API key for Steam API.
+    
+    Returns:
+    - steam_user_id (str): The Steam user ID.
+    """
+
+    print("Welcome to the Steam Data Extractor!")
+    print("This tool allows you to extract Steam user data and game achievements.\n")
+    print("Please select how you want to identify the user:")
+
+    while True:
+        try:
+            input_option = int(input('Select an option:\n1. By Steam ID\n2. By Vanity URL\n'))
+            
+            if input_option in(1, 2):
+                break
+            else:
+                print('Invalid option. Please select 1 or 2.')
+        except ValueError:
+            print('Invalid input. Please enter a number.')
+
+    if input_option == 1:
+        steam_user_id = input('Enter the Steam ID (ex 7656119...): ')
+        print(f'You selected to identify by Steam ID: {steam_user_id}')
+        return steam_user_id
+    elif input_option == 2:
+        vanity_name = input('Please enter the name present in your profile URL: ')
+        vanity_url = f'https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={api_key}&vanityurl={vanity_name}'
+
+        response = make_request(vanity_url, retry=4, wait=2)
+
+        if response is not None and response.status_code == 200:
+            json_vanity_info = response.json()
+            if json_vanity_info.get('response').get('success') == 1:
+                steam_user_id = json_vanity_info.get('response').get('steamid')
+                return steam_user_id
+        else:
+            print(f'Error: Vanity URL not found or does not exist. Please check the vanity name: {vanity_name} or use your Steam ID.')
+            return None
+        
+#----------------------------------------------------------------------
+
+def get_user_info(steam_user_id, api_key):
+    """
+    Get user information from Steam API by user ID.
+    
+    Parameters:
+    - steam_user_id (str): The Steam user ID.
+    - api_key (str): The API key for Steam API.
+    
+    Returns:
+    - json_user_info (dict): The JSON response containing user information.
+    """
+    
+    url = f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={api_key}&steamids={steam_user_id}'
+    response = make_request(url, retry=4, wait=2)
+
+    if response is not None and response.status_code == 200:
+        json_user_info = response.json()
+        return json_user_info
+    else:
+        print(f'Error fetching user information for Steam ID {steam_user_id}. Please check the ID or your API key.')
+        return None
+    
+
+#----------------------------------------------------------------------
+
+def get_owned_games(steam_user_id, api_key):
+    """
+    Get the list of owned games by a Steam user.
+    
+    Parameters:
+    - steam_user_id (str): The Steam user ID.
+    - api_key (str): The API key for Steam API.
+    
+    Returns:
+    - json_games_info (dict): The JSON response containing game information.
+    """
+    
+    games_info_url = f'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={steam_user_id}&include_appinfo=true&include_played_free_games=true&format=json'
+    
+    response = make_request(games_info_url, retry=4, wait=2)
+
+    if response is not None and response.status_code == 200:
+        json_games_info = response.json()
+        return json_games_info
+    else:
+        print(f'Error fetching owned games for Steam ID {steam_user_id}. Please check the ID or your API key.')
+        return None
+    
+
+#----------------------------------------------------------------------
+
+def get_game_achievements(steam_user_id, api_key, appid):
+    """
+    Get the achievements for a specific game by appid.
+    
+    Parameters:
+    - steam_user_id (str): The Steam user ID.
+    - api_key (str): The API key for Steam API.
+    - appid (int): The application ID of the game.
+    
+    Returns:
+    - json_game_achievements (dict): The JSON response containing game achievements.
+    """
+    
+    game_achievements_url = f'http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid={appid}&key={api_key}&steamid={steam_user_id}&l=en'
+    response = make_request(game_achievements_url, retry=4, wait=2)
+
+    if response is not None and response.status_code == 200:
+        json_game_achievements = response.json()
+        return json_game_achievements
+    else:
+        print(f'Error fetching achievements for appid {appid}. Please check the appid or your API key.')
+        return None
+    
+#----------------------------------------------------------------------
+
+def save_file_opt(df_final, file_name):
+
+    while True:
+        option = int(input("Select an option:\n1. Save as CSV\n2. Save as Excel\n3. Save as JSON\n"))
+        if option in [1, 2, 3]:
+            break
+        else:
+            print("Invalid option. Please select 1, 2, or 3.")
+
+    cust_path = input("Do you want to save the file in a custom path? (yes/no): ").lower()
+
+    root = tk.Tk()
+
+    if cust_path == 'yes':
+        file_path = filedialog.askdirectory(title="Select a directory")
+        print(f"Selected directory: {file_path}")
+    else:
+        print("Using current directory for saving files.")
+
+
+    if option == 1:
+        if cust_path == 'yes':
+            df_final.to_csv(f"{file_path}/{file_name}.csv", index=False)
+        else:
+            df_final.to_csv(f"{file_name}.csv", index=False)
+    elif option == 2:
+        if cust_path == 'yes':
+            df_final.to_excel(f"{file_path}/{file_name}.xlsx", index=False)
+        else:
+            df_final.to_excel(f"{file_name}.xlsx", index=False)
+    elif option == 3:
+        if cust_path == 'yes':
+            df_final.to_json(f"{file_path}/{file_name}.json", orient='records', lines=True)
+        else:
+            df_final.to_json(f"{file_name}.json", orient='records', lines=True)
+
+#-----------------------------------------------------------------------
+
+def ai_achievement_breakdown(ai_api_key, steam_user_id, API_KEY):
+    """
+    Function to interact with OpenAI API to get achievement breakdown.
+    """
+
+    games_info = get_owned_games(steam_user_id, API_KEY).get('response').get('games')
+
+    game_list = []
+
+    for game in games_info:
+        game_list.append(
+            {
+                "name": game['name'],
+                "steam_game_id": game['appid']
+            }
+        )
+
+    game_df = pd.DataFrame(game_list)
+
+    game_df.sort_values(by='name')
+
+    options = game_df['name'].to_list()
+
+    selected_option = questionary.select(
+        "Select a game to view unlocked achievements breakdonw:",
+        choices=options
+    ).ask()
+
+    appid = game_df[game_df['name'] == selected_option]['steam_game_id'].iloc[0]
+
+    print(f"You selected: {selected_option}({appid})")
+
+    json_game_breakdown = get_game_achievements(steam_user_id, API_KEY, appid)
+
+    client = OpenAI(
+        api_key = ai_api_key
+    )
+
+    prompt = f"""
+    You are a famous and respected video game journalist who specializes in helping players achieve 100% completion, platinum trophies, or 1000G achievements.
+
+    I will provide you with:
+
+    - The name of a game
+    - A JSON file containing all achievements in this game, indicating which ones the player has unlocked and which ones remain locked.
+
+    unlocked achievements is set to true, and locked achievements is set to false.
+
+    For each locked achievement, please provide:
+
+    1. The achievement name.
+    2. A brief summary of what the player needs to do to unlock it.
+    3. Helpful tips to achieve it.
+
+    If an achievement has no description in the JSON, search for official information on the internet (e.g., game wikis, official sources).  
+    - If you find reliable info, include it as the summary and tips.  
+
+    for each achievement, please provide the information in the following format:
+    - Achievement Name: [Achievement Name]
+    - Summary: [Brief summary of what the player needs to do to unlock it]
+    - Tips: [Helpful tips to achieve it]
+
+    Please provide your response clearly and concisely, as if you are preparing a guide to be shown inside a gaming app.
+
+    Here is the game name and the JSON data:
+
+    Game: {selected_option}
+
+    Achievements JSON:  
+    {json_game_breakdown.get('playerstats').get('achievements')}
+    """
+
+    completion = client.chat.completions.create(
+        model=os.getenv("GPT_MODEL"),
+        store=True,
+        temperature=0.5,
+        max_tokens=800,
+        messages=[
+            {"role": "user", "content": prompt }
+        ]
+    )
+
+    content = completion.choices[0].message.content
+    print(content)
+
+#---------------------------------- Send data to database ----------------------------------#
+
+def save_to_sqlite(df, table_name, method ):
+    """
+    Save DataFrame to SQLite database.
+    
+    Parameters:
+    - df (pd.DataFrame): The DataFrame to save.
+    - method (str): The method to use when saving ('append' or 'replace').
+    """
+
+    conn = sqlite3.connect('database/steam_data_raw.db')
+
+    try:
+        print(f'Checking if table {table_name} exists...')
+        df.head(0).to_sql(table_name, conn, if_exists='fail', index=False)  # Create table
+    except Exception as e:
+        print(f'Table {table_name} already exists. Proceeding to insert data.')
+
+    print(f'Saving data to table {table_name} using method {method}... {len(df)} records to be inserted.')
+
+    try:
+        print(f'Inserting data into table {table_name}...')
+        df.to_sql(table_name, conn, if_exists=method, index=False)
+    except Exception as e:
+        print(f'Error inserting data into table {table_name}: {e}')
+
+    conn.close()
